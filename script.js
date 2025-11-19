104f4c3b9de29183b2fd2a6ea6121078796bd3da
Lukinhas019 <Lukinhass.assis889@outlook.com>
2025-11-18 23:00:14 -0300

    Foi adicionado outro selec na opção de converter

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const converterButton = document.querySelector('.converter-button')
 const optionSelect = document.querySelector('.option-select')
+const optionSelect1 = document.querySelector('.option-select-1')
 
 
 
@@ -11,16 +12,53 @@ function ConvertValues() {
 
 
 
+
     const DolarValor = 5.33
     const EuroValor = 6.18
+    const LibraValor = 6.99
+    const BtcValor = 490.705
+    const RealValor = 1
 
 
-    resultadoReal.innerHTML = new Intl.NumberFormat("pt-BR", {
-        style: "currency",
-        currency: "BRL"
+    if (optionSelect1.value == "real1") {
+        resultadoReal.innerHTML = new Intl.NumberFormat("pt-Br", {
+            style: "currency",
+            currency: "BRL"
+        }).format(inputValor / RealValor)
+    }
 
-    }).format(inputValor)
 
+    if (optionSelect1.value == "dolar1") {
+        resultadoReal.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+
+        }).format(inputValor / DolarValor)
+    }
+
+    if (optionSelect1.value == "euro1") {
+
+        resultadoReal.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputValor / EuroValor)
+    }
+
+    if (optionSelect1.value == "libra1") {
+
+        resultadoReal.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: "currency",
+            currency: "GBP"
+        }).format(inputValor / LibraValor)
+    }
+
+    if (optionSelect1.value == "btc1") {
+
+        resultadoReal.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "BTC"
+        }).format(inputValor / BtcValor)
+    }
 
 
 
@@ -40,6 +78,28 @@ function ConvertValues() {
         }).format(inputValor / EuroValor)
     }
 
+    if (optionSelect.value == "libra") {
+
+        resultadoDolar.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: "currency",
+            currency: "GBP"
+        }).format(inputValor / LibraValor)
+    }
+
+    if (optionSelect.value == "btc") {
+
+        resultadoDolar.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "BTC"
+        }).format(inputValor / BtcValor)
+    }
+
+    if (optionSelect.value == "real") {
+        resultadoDolar.innerHTML = new Intl.NumberFormat("pt-Br", {
+            style: "currency",
+            currency: "BRL"
+        }).format(inputValor / RealValor)
+    }
 
 
 }
@@ -47,18 +107,73 @@ function ConvertValues() {
 function change() {
 
     const styleDolar = document.querySelector('.style-dolar')
+    const styleReal = document.querySelector('.style-real')
     const imgChange = document.querySelector('.img-dolar')
-
-    if (optionSelect.value == "dolar") {
-        styleDolar.innerHTML = "Dólar Americano"
-        imgChange.src = './assets/dolar.png'
+    const imgBrasil = document.querySelector('.img-brasil')
 
 
-    }
+
 
     if (optionSelect.value == "euro") {
         styleDolar.innerHTML = "Euro"
         imgChange.src = "./assets/euro.png"
+
+    }
+
+
+
+    if (optionSelect.value == "libra") {
+        styleDolar.innerHTML = "Libra"
+        imgChange.src = "./assets/libra.png"
+
+    }
+
+    if (optionSelect.value == "btc") {
+        styleDolar.innerHTML = "BTC"
+        imgChange.src = "./assets/bitcoin.png"
+
+    }
+
+    if (optionSelect.value == "real") {
+        styleDolar.innerHTML = "Real"
+        imgChange.src = './assets/real.png'
+
+    }
+
+
+
+
+
+
+    if (optionSelect1.value == "real1") {
+        styleReal.innerHTML = "Real"
+        imgBrasil.src = './assets/real.png'
+
+    }
+
+
+    if (optionSelect1.value == "dolar1") {
+        styleReal.innerHTML = "Dólar Americano"
+        imgBrasil.src = './assets/dolar.png'
+
+
+    }
+
+    if (optionSelect1.value == "euro1") {
+        styleReal.innerHTML = "Euro"
+        imgBrasil.src = "./assets/euro.png"
+
+    }
+
+    if (optionSelect1.value == "libra1") {
+        styleReal.innerHTML = "Libra"
+        imgBrasil.src = "./assets/libra.png"
+
+    }
+
+    if (optionSelect1.value == "btc1") {
+        styleReal.innerHTML = "BTC"
+        imgBrasil.src = "./assets/bitcoin.png"
 
     }
 
@@ -70,3 +185,4 @@ function change() {
 
 converterButton.addEventListener("click", ConvertValues)
 optionSelect.addEventListener("change", change)
+optionSelect1.addEventListener("change", change)
